a2fc1ca000ec6920614cc7dd2b650177a4a84fcd
Gabox787 <arsgorenko@gmail.com>
2026-03-10 19:23:57 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Telegram настройки
+TELEGRAM_TOKEN = '8530434492:AAHE7S6GHnLImGUIkpyqeKrr4slOwR5gIDU'
+CHAT_ID = '715162339'
+
+# Настройки сетки
+SYMBOL = "BTCUSDT"
+GRID_STEP = 0.5  # Шаг 0.5%
+GRID_LEVELS = 3  # По 3 уровня вверх и вниз
